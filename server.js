02f8908f59e06c52c29ecdb18ebef7f5f30fcc52
JRM <johnrhmartin@gmail.com>
2022-04-02 16:57:26 -0400

    added mysql2 and createConnection(0 for the db

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mysql = require('mysql2');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -6,11 +8,30 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Default response for any other request (Not Found)
-app.use((req,res) => {
-    res.status(400).end();
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      port: '3306',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'password',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+    console.log(err);
 });
 
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+  
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
